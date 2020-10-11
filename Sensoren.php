da4a5b82929670ec160e818772f21bb43d265e39
11902804 <61013263+11902804@users.noreply.github.com>
2020-10-11 15:06:03 +0200

    Add files via upload

---
+++ Sensoren.php
@@ -0,0 +1,34 @@
+<!DOCTYPE php>
+<?php
+$servername = "localhost";
+$username = "student_11902804";
+$password = "eJ9z69mNneiR";
+$dbname = "student_11902804";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$Waarde = filter_input(INPUT_POST, 'Waarde');
+$ID = filter_input(INPUT_POST, 'ID');
+
+if (!empty($Waarde)){
+if (!empty($ID)){
+$host = "localhost";
+
+$sql = "INSERT INTO Data (Waarde, ID)
+VALUES ('$Waarde','$ID')";
+}}
+
+if ($conn->query($sql) === TRUE) {
+  echo "OK <br>" ;//. $sql;
+} 
+else {
+  echo "NOK" . $sql . "<br>" . $conn->error;
+}
+$sql = "UPDATE Sensoren SET ID='ID' WHERE nr=1";
+
+$conn->close();
+?>
