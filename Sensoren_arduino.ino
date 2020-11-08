60f163f9697c906f0524f99065d040cbc151a0b6
11902804 <61013263+11902804@users.noreply.github.com>
2020-11-08 16:42:24 +0100

    Update Sensoren_arduino.ino

--- Sensoren_arduino.ino
+++ Sensoren_arduino.ino
@@ -1,7 +1,7 @@
 const int ledPin = 13;
-
+int ID = 0;
 const int ldrPin = A0;
-float temp;
+int temp;
 const int tempPin = A2;
 
 void setup() {
@@ -22,39 +22,43 @@ if (ldrStatus <= 200) {
 
 digitalWrite(ledPin, HIGH);
 
-Serial.print("Its DARK!");
-
-Serial.println(ldrStatus);
-delay(2000); 
+Serial.print(ldrStatus);
+Serial.print('\n');
+ID = 2;
+Serial.print(ID);
+Serial.print('\n');
+delay(20000); 
 temp = analogRead(tempPin);
    
 temp = temp * 0.48828125;
-   
-Serial.print("TEMPERATURE = ");
-Serial.print(temp); 
-Serial.print("°C");
-Serial.println();
-delay(3000); 
+Serial.print(temp);
+Serial.print('\n'); 
+ID = 1;
+Serial.print(ID);
+Serial.print('\n');
+delay(30000); 
 
 } else {
 
 digitalWrite(ledPin, LOW);
 
-Serial.print("Its BRIGHT!");
-
-Serial.println(ldrStatus);
-delay(2000);
+Serial.print(ldrStatus);
+Serial.print('\n');
+ID = 2;
+Serial.print(ID);
+Serial.print('\n');
+delay(20000);
 
 temp = analogRead(tempPin);
 temp = temp * 0.48828125;
-Serial.print("TEMPERATURE = ");
 Serial.print(temp); 
-Serial.print("°C");
-Serial.println();
-delay(3000);
+Serial.print('\n');
+ID = 1;
+Serial.print(ID);
+Serial.print('\n');
+delay(30000);
 }
 }
-
 
 
 
