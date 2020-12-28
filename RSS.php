d451c7156541469bfa24ec6804a4716b419fdc0b
11902804 <61013263+11902804@users.noreply.github.com>
2020-12-28 13:47:17 +0100

    Add files via upload

--- RSS.php
+++ RSS.php
@@ -17,7 +17,8 @@ $mintemp = $res['min'];
 $sql= mysqli_query($con, "SELECT AVG( Waarde ) AS avg FROM Data WHERE ID = 1;");
 $res = mysqli_fetch_array( $sql);
 $avgtemp = $res['avg'];
-$sql="SELECT * FROM Data";
+$sql="SELECT * FROM Data WHERE ID = 1";
+$result = mysqli_query($con,$sql);
 while($row = mysqli_fetch_array($result)) {	
 	$laatstetemp=$row['Waarde'];
 	$laatsteTijd=$row['Tijd'];
@@ -28,50 +29,49 @@ $res = mysqli_fetch_array( $sql);
 $maxlicht = $res['max2'];
 $sql= mysqli_query($con, "SELECT MIN( Waarde ) AS min2 FROM Data WHERE ID =2;" );
 $res = mysqli_fetch_array( $sql);
-$minlicht = $res['min1'];
+$minlicht = $res['min2'];
 $sql= mysqli_query($con, "SELECT AVG( Waarde ) AS avg2 FROM Data WHERE ID = 2;");
 $res = mysqli_fetch_array( $sql);
 $avglicht = $res['avg2'];
-$sql="SELECT * FROM Data";
+$sql="SELECT * FROM Data WHERE ID = 1";
+$result = mysqli_query($con,$sql);
 while($row = mysqli_fetch_array($result)) {	
 	$laatstelicht=$row['Waarde'];
 	$laatsteTijd2=$row['Tijd'];
 }
+header('Content-type: text/xml;charset=iso-8859-1');
+echo "<?xml version='1.0'  encoding='UTF-8' ?> ";"<br>";
+echo "<rss version='2.0'>";"<br>";
+echo "<channel>";"<br>";
+
+echo "<title>RSS Feed</title>";"<br>";
+
+echo "<title>TemperatuurSensor</title>";"<br>";
+echo "<title>gemiddelde</title>";"<br>";
+echo "<description>".$avgtemp."</description>";"<br>";
+echo "<title>Laatste tijd</title>";"<br>";
+echo "<description>".$laatsteTijd."</description>";"<br>";
+echo "<title>Laatste waarde</title>";"<br>";
+echo "<description>".$laatstetemp."</description>";"<br>";
+echo "<title>minimum</title>";"<br>";
+echo "<description>".$mintemp."</description>";"<br>";
+echo "<title>maximum</title>";"<br>";
+echo "<description>".$maxtemp."</description>";"<br>";
+echo "<title>LichtSensor</title>";"<br>";
+echo "<title>Laatste waarde</title>";"<br>";
+echo "<description>".$laatstelicht."</description>";"<br>";
+echo "<title>Laatste tijd</title>";"<br>";
+echo "<description>".$laatsteTijd2."</description>";"<br>";
+echo "<title>gemiddelde</title>";"<br>";
+echo "<description>".$avglicht."</description>";"<br>";
+echo "<title>minimum</title>";"<br>";
+echo "<description>".$minlicht."</description>";"<br>";
+echo "<title>maximum</title>";"<br>";
+echo "<description>".$maxlicht."</description>";"<br>";
+
+
+echo "</channel>";"<br>";
+echo "</rss>";"<br>";
+
 mysqli_close($con);
-
-header("Content-type: text/xml"); 
-
-echo "<?xml version='1.0' encoding='UTF-8'?>
-<rss version='2.0'>
-<channel>
-<title>RSS Feed</title>
-<item>
-<title>Temperatuur</title>
-<title>maximum</title>
-<description>"$maxtemp"</description>
-<title>minimum</title>
-<description>"$mintemp"</description>
-<title>gemiddelde laatste 24 uur</title>
-<description>"$avgtemp"</description>
-<title>nieuwste Waarde</title>
-<description>"$laatstetemp"</description>
-<title>nieuwste tijdstempel</title>
-<description>"$laatsteTijd"</description>
-</item>
-
-<item>
-<title>Licht</title>
-<title>maximum</title>
-<description>"$maxlicht"</description>
-<title>minimum</title>
-<description>"$minlicht"</description>
-<title>gemiddelde laatste 24 uur</title>
-<description>"$avglicht"</description>
-<title>nieuwste Waarde</title>
-<description>"$laatstelicht"</description>
-<title>nieuwste tijdstempel</title>
-<description>"$laatsteTijd2"</description>
-</item>
-</channel></rss>"; 
-
 ?>
